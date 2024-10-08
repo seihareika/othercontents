b450ff94876ecb7219f5c3750b9f8c5d08454edf
seihareika <133826539+seihareika@users.noreply.github.com>
2024-10-08 15:29:47 +0900

    Add files via upload

---
+++ other1/etc/web/10js/header_depth1.js
@@ -0,0 +1,25 @@
+document.write(`
+    <header>
+    <h1></h1><h2></h2>
+    <div class="openbtn"><span></span><span></span><span></span></div>
+    <nav id="g-nav">
+        <div id="g-nav-list">
+            <ul>
+                <li><a href=""><h1>TOP</h1></a></li>
+                <br><hr><br>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+                <li><a href="/"><h3></h3></a></li>
+            </ul>
+        </div>
+    </nav>
+    <a href="#" class="page_top_btn">上へ戻る</a>
+    </header>
+    `);
